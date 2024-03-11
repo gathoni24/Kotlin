87f416c29c2e6d34621f6600641c7cc87ab21d05
gathoni24 <traceygathonii007@gmail.com>
2024-03-11 13:07:59 +0300

    commit

---
+++ src/introduction.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("Tracey")
+    println(20)
+    println(20+30)
+    println("eMobilis")
+    print("Tracey")
+    print("Gathoni")
+}
---
+++ src/variables.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var firstname = "Tracey"
+    firstname = "June"
+
+    val pi =3.14
+    var lastname = "Gathoni"
+    var school = "eMobilis"
+
+    print(firstname)
+    print(" ")
+    print(lastname)
+    print(" ")
+    println(school)
+
+    println("My firstname is" + " " + firstname + "." + "I study at" + " "+ school)
+}
